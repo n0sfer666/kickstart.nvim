902ce23298c358c6d6fe6ef1e4fa1f6d8669599e
Khasanov Petr <petr.khasanov@metalamp.io>
2025-04-17 16:10:27 +0300

    fixed spectre backup issue #118

--- lua/custom/plugins/spectre.lua
+++ lua/custom/plugins/spectre.lua
@@ -2,7 +2,18 @@ return {
   {
     'nvim-pack/nvim-spectre',
     config = function()
-      require('spectre').setup()
+      require('spectre').setup {
+        replace_engine = {
+          ['sed'] = {
+            cmd = 'sed',
+            args = {
+              '-i',
+              '',
+              '-E',
+            },
+          },
+        },
+      }
       vim.keymap.set('n', '<leader>rr', function()
         require('spectre').open()
       end, { desc = 'Replace in files (Spectre)' })

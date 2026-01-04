40a5fdc543edaf4eded2b7d37bb12d666ef4e51c
Khasanov Petr <petr.khasanov@metalamp.io>
2026-01-04 19:09:27 +0300

    added stylelint solving issue

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,8 +6,6 @@ return {
       local on_attach = function(_, bufnr)
         local opts = { noremap = true, silent = true, buffer = bufnr }
         vim.keymap.set('n', '<leader>cn', vim.lsp.buf.code_action, vim.tbl_extend('force', opts, { desc = 'LSP Code Actions' }))
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, vim.tbl_extend('force', opts, { desc = 'Go to Definition' }))
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, vim.tbl_extend('force', opts, { desc = 'Hover Docs' }))
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, vim.tbl_extend('force', opts, { desc = 'Rename Symbol' }))
       end
 
@@ -23,13 +21,13 @@ return {
           format = { enable = true },
         },
       }
-      vim.lsp.enable('eslint')
+      vim.lsp.enable 'eslint'
 
       vim.lsp.config.ts_ls = {
         cmd = { 'typescript-language-server', '--stdio' },
         root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json' },
       }
-      vim.lsp.enable('ts_ls')
+      vim.lsp.enable 'ts_ls'
 
       vim.lsp.config.jsonls = {
         cmd = { 'vscode-json-language-server', '--stdio' },
@@ -40,7 +38,22 @@ return {
           },
         },
       }
-      vim.lsp.enable('jsonls')
+      vim.lsp.enable 'jsonls'
+
+      if vim.fn.executable 'stylelint-lsp' == 1 then
+        vim.lsp.config.stylelint_lsp = {
+          cmd = { 'stylelint-lsp', '--stdio' },
+          filetypes = { 'css', 'scss', 'sass', 'less' },
+          root_markers = { '.stylelintrc', '.stylelintrc.json', '.stylelintrc.js', 'stylelint.config.js', '.stylelintrc.yml' },
+          settings = {
+            stylelintplus = {
+              autoFixOnSave = false,
+              autoFixOnFormat = false,
+            },
+          },
+        }
+        vim.lsp.enable 'stylelint_lsp'
+      end
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
